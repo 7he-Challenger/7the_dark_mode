4e6d2cfe65dbb7afdb9791782d9893543c534b75
Rovapicolino <rova@picolinoagency.com>
2022-07-12 10:38:34 +0300

    initial commit

--- Rova/js/main.js
+++ Rova/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    checkMode();
    $("#darkSwitch").change(function(){
         if( $("#darkSwitch").is(":checked")){
             changeMode("dark");
@@ -9,6 +10,7 @@ $(document).ready(function(){
 
    function changeMode(type){
     if(type == "white"){
+        setMode("DarkMode", "0", 1);
         var toRemove = "dark";
         var toAdd = "white";
             document.getElementsByClassName('white-logo')[0].classList.add("showLogo");
@@ -17,6 +19,7 @@ $(document).ready(function(){
             document.getElementsByClassName('dark-logo')[0].classList.add("hideLogo");
             document.getElementsByClassName('dark-logo')[0].classList.remove("showLogo");
     }else{
+        setMode("DarkMode", "1", 1);
         var toRemove = "white";
         var toAdd = "dark";
             document.getElementsByClassName('white-logo')[0].classList.remove("showLogo");
@@ -36,4 +39,45 @@ $(document).ready(function(){
         }
     }
    }
+
+
+
+   function setMode(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getMode(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  
+  function checkMode() {
+    let darkMode = getMode("DarkMode");
+    if (darkMode != "") {
+        if(darkMode != "1"){
+            changeMode("white");
+            setMode("DarkMode", "0", 365);
+        }else{
+            $("#darkSwitch").attr('checked','checked')
+            changeMode("dark");
+            setMode("DarkMode", "1", 365);
+        }
+    } else {
+        changeMode("white");
+        setMode("DarkMode", "0", 365);
+    }
+  }
 })
