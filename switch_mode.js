4532374345c4ebedd576836194a34a9478dbf9b1
Mitantsoaa <mitaranaivo@gmail.com>
2022-07-12 15:39:56 +0300

    Correct style

--- switch_mode.js
+++ switch_mode.js
@@ -2,8 +2,14 @@ $(document).ready(function(){
     $('#dark-mode').change(function () {
         if ($('#dark-mode').prop('checked') == true) {
             $('body').addClass('dark');
+            $('body').removeClass('light');
+            $('.label-light').show();
+            $('.label-dark').hide();
         } else {
             $('body').removeClass('dark');
+            $('body').addClass('light');
+            $('.label-dark').show();
+            $('.label-light').hide();
         }
     });
 });
